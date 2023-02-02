4e1a742b511bd1755f25982b5638f9d5723a8e37
Steven <hsteven95@hotmail.co.uk>
2023-02-02 22:52:31 +1000

    Minor Update: added more icons

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,51 +1,63 @@
 import { Flex, Icon } from "@chakra-ui/react";
 import React from "react";
-import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
-import { GrAdd } from "react-icons/gr";
-import {
-  IoFilterCircleOutline,
-  IoNotificationsOutline,
-  IoVideocamOutline,
-} from "react-icons/io5";
+import { BsGraphUp, BsChatDots, BsBookmarkHeart } from "react-icons/bs";
+import { GoKeyboard } from "react-icons/go";
+//TODO maybe replace the login and logout with react-icons
+import { VscSignIn, VscSignOut } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
 
 const Icons: React.FC = () => {
   return (
-    <Flex
-      display={{ base: "none", md: "flex" }}
-      align="center"
-      borderRight={"1px solid"}
-      borderColor="gray.200"
-    >
+    <Flex>
       <Flex
-        mr={1.5}
-        ml={1.5}
-        padding={1}
-        cursor="pointer"
-        borderRadius={4}
-        _hover={{ bg: "gray.200" }}
+        display={{ base: "none", md: "flex" }}
+        align="center"
+        borderRight={"1px solid"}
+        borderColor="gray.200"
       >
-        <Icon as={BsArrowUpRightCircle} fontSize={20}/>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={BsGraphUp} fontSize={19} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={BsBookmarkHeart} fontSize={20} />
+        </Flex>
       </Flex>
-      <Flex
-        mr={1.5}
-        ml={1.5}
-        padding={1}
-        cursor="pointer"
-        borderRadius={4}
-        _hover={{ bg: "gray.200" }}
-      >
-        <Icon as={IoFilterCircleOutline} fontSize={22}/>
-      </Flex>
-      <Flex
-        mr={1.5}
-        ml={1.5}
-        padding={1}
-        cursor="pointer"
-        borderRadius={4}
-        _hover={{ bg: "gray.200" }}
-      >
-        <Icon as={IoVideocamOutline} fontSize={22}/>
-      </Flex>
+      <>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={GoKeyboard} fontSize={21} />
+        </Flex>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={CgProfile} fontSize={20} />
+        </Flex>
+      </>
     </Flex>
   );
 };
